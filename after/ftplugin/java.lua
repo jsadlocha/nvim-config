b0d5c7a00c23ca18b937e96994028f5889eab9c1
jsadlocha <jr.sadlocha@gmail.com>
2023-09-06 22:31:45 +0200

    Add DAP support for java

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,6 +1,16 @@
+local bundles = {
+  vim.fn.glob('~/.install/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.49.0.jar')
+}
+
 local config = {
     cmd = {'/home/hardware/.config/jdtls/bin/jdtls'},
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+    on_attach = function(client, bufnr)
+      require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+    end,
+    init_options = {
+      bundles = bundles
+    },
 }
 require('jdtls').start_or_attach(config)
 
--- lua/config/nvim-dap.lua
+++ lua/config/nvim-dap.lua
@@ -77,6 +77,14 @@ dap.configurations.cpp = {
   },
 }
 
+dap.configurations.java = {
+  {
+    type = 'java',
+    request = 'launch',
+    name = 'Attach to the process',
+  },
+}
+
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
 
@@ -84,10 +92,10 @@ dap.listeners.after.event_initialized["dapui_config"]=function()
   dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"]=function()
-  -- dapui.close()
+  dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"]=function()
-  -- dapui.close()
+  dapui.close()
 end
 
 vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
