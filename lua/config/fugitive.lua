c0ea4a1b11ec77a2b3afd247ef5eda964f87d775
jsadlocha <jr.sadlocha@gmail.com>
2023-09-06 18:35:46 +0200

    Change abbreviation for git add

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -1,7 +1,7 @@
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git status" })
-keymap.set("n", "<leader>gw", "<cmd>Gwrite<cr>", { desc = "Git add" })
+keymap.set("n", "<leader>ga", "<cmd>Gwrite<cr>", { desc = "Git add" })
 keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "Git commit" })
 keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", { desc = "Git diff" })
 keymap.set("n", "<leader>gpl", "<cmd>Git pull<cr>", { desc = "Git pull" })
