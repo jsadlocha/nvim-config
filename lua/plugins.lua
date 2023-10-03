d076a72a6c2bd02964a759c13574b998966b79fe
jsadlocha <jr.sadlocha@gmail.com>
2023-10-03 07:23:32 +0200

    Add multicursors plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -512,5 +512,24 @@ require("lazy").setup {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope.nvim"
       }
-  }
+  },
+
+  -- Multicursors
+  {
+      "smoka7/multicursors.nvim",
+      event = "VeryLazy",
+      dependencies = {
+          'smoka7/hydra.nvim',
+      },
+      opts = {},
+      cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+      keys = {
+              {
+                  mode = { 'v', 'n' },
+                  '<Leader>m',
+                  '<cmd>MCstart<cr>',
+                  desc = 'Create a selection for selected text or word under the cursor',
+              },
+          },
+  },
 }
